1060baac7b3ba3df90ed7a3575108b448b3bea7b
Jeon Ye-Chan <skyvvv624@naver.com>
2016-11-13 00:02:16 +0900

    add

---
+++ win32api_output2/win32api_output2/main.cpp
@@ -0,0 +1,60 @@
+#include <Windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPWSTR lpszClass = TEXT("Output Example2");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&WndClass);
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, 0, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		return 0;
+	case WM_LBUTTONDOWN:
+		if (MessageBox(hWnd, TEXT("왼쪽 버튼을 클릭하였습니다."), TEXT("메시지 박스"), MB_YESNO) == IDYES) 
+		{
+			MessageBox(hWnd, TEXT("\"예\" 버튼을 클릭하였습니다."), TEXT("메시지 박스"), MB_OK);
+		}
+		else
+		{
+			MessageBox(hWnd, TEXT("\"아니오\" 버튼을 클릭하였습니다."), TEXT("메시지 박스"), MB_OK);
+		}
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
